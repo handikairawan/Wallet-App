e490e65edd8e760d3bb7ac11bf16cddf908b3a1c
Handika Irawan <handikairawan@gmail.com>
2020-06-05 16:20:00 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallet App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('Chart'),
+                color: Colors.redAccent,
+                elevation: 10,
+              ),
+            ),
+            Card(
+              color: Colors.teal,
+              child: Text('List Of Transaction'),
+            )
+          ],
+        ));
+  }
+}
