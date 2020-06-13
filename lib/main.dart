d20be0dc48a099ef76f7640993e5425e62998ace
Handika Irawan <handikairawan@gmail.com>
2020-06-13 08:55:31 +0700

    update transaction style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +16,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+        id: '1', title: 'Order GoFood', amount: 20.000, date: DateTime.now()),
+    Transaction(
+        id: '2', title: 'Top Up OVO', amount: 50.000, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,10 +41,44 @@ class MyHomePage extends StatelessWidget {
                 elevation: 10,
               ),
             ),
-            Card(
-              color: Colors.teal,
-              child: Text('List Of Transaction'),
-            )
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.teal, width: 2)),
+                        padding: EdgeInsets.all(5),
+                        child: Text(
+                          tx.amount.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.teal),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           ],
         ));
   }
